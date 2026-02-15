52091e321b4719d43177c63e939f8adf94a52bb0
tbv001 <sebuahakun123@gmail.com>
2026-02-15 21:03:00 +0700

    Weapons database

--- lua/autorun/server/sv_mbot.lua
+++ lua/autorun/server/sv_mbot.lua
@@ -1,6 +1,7 @@
 if game.SinglePlayer() or engine.ActiveGamemode() ~= "morbusgame" then return end
 
 MBot = {}
+include("mbot/database.lua")
 include("mbot/states.lua")
 include("mbot/cmd.lua")
 include("mbot/func.lua")
---
+++ lua/mbot/database.lua
@@ -0,0 +1,59 @@
+--[[----------------------------------------
+    Pistol Weapons
+----------------------------------------]]--
+
+MBot.Database.WEAPON_PISTOL = {
+    ["weapon_mor_beretta"] = true,
+    ["weapon_mor_blaster"] = true,
+    ["weapon_mor_glock"] = true,
+    ["weapon_mor_usp"] = true,
+    ["weapon_mor_p229"] = true,
+    ["weapon_mor_phaser"] = true
+}
+
+
+--[[----------------------------------------
+    Light/SMG Weapons
+----------------------------------------]]--
+
+MBot.Database.WEAPON_LIGHT = {
+    ["weapon_mor_ump"] = true,
+    ["weapon_mor_uzi"] = true,
+    ["weapon_mor_r22"] = true,
+    ["weapon_mor_vek"] = true,
+    ["weapon_mor_teslar"] = true,
+    ["weapon_mor_pulsesmg"] = true
+}
+
+
+--[[----------------------------------------
+    Rifle Weapons
+----------------------------------------]]--
+
+MBot.Database.WEAPON_RIFLE = {
+    ["weapon_mor_sc7"] = true,
+    ["weapon_mor_zx9"] = true,
+    ["weapon_mor_bulldog"] = true,
+    ["weapon_mor_teslar"] = true,
+    ["weapon_mor_mars"] = true,
+    ["weapon_mor_lz400"] = true,
+    ["weapon_mor_scarm8"] = true,
+    ["weapon_mor_gr4"] = true,
+    ["weapon_mor_m20"] = true,
+    ["weapon_mor_ka47"] = true,
+    ["weapon_mor_m418"] = true
+}
+
+
+--[[----------------------------------------
+    Misc Weapons
+----------------------------------------]]--
+
+MBot.Database.WEAPON_MISC = {
+    ["weapon_frag"] = true,
+    ["weapon_glowstick"] = true,
+    ["weapon_mor_medkit"] = true,
+    ["weapon_glowstick_grav"] = true,
+    ["weapon_glowstick_sticky"] = true,
+    ["weapon_glowstick_special"] = true
+}
